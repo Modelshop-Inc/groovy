3bea4e4bf6d8c51e76585ae0b82d7b3c96eaa6c0
tomtobin_modelshop <tomtobin@modelshop.com>
2020-11-12 19:18:59 -0500

    fuck unique

---
+++ install.sh
@@ -0,0 +1,4 @@
+mkdir -p ../../modelshop-master/modelshop-maven/org/codehaus/groovy/groovy/2.5.6$1
+cp ./target/libs/groovy-2.5.6-indy.jar ../../modelshop-master/modelshop-maven/org/codehaus/groovy/groovy/2.5.6$1/groovy-2.5.6$1-indy.jar
+cp ./target/libs/groovy-2.5.6-sources.jar ../../modelshop-master/modelshop-maven/org/codehaus/groovy/groovy/2.5.6$1/groovy-2.5.6$1-sources.jar
+rm C:/Users/tomtobin/.m2/repository/org/codehaus/groovy/groovy/2.5.6$1/groovy-2.5.6$1-indy.jar
